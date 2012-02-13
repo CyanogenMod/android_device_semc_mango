046dac86d307a1a57d74a81164752693cdea8656
freexperia <jerpelea@gmail.com>
2012-02-13 22:12:38 +0200

    semc: update with latest changes

--- device_mango.mk
+++ device_mango.mk
@@ -11,6 +11,10 @@ PRODUCT_NAME := mango
 PRODUCT_DEVICE := mango
 PRODUCT_MODEL := mango
 
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
+PRODUCT_COPY_FILES += \
+    $(TARGET_PREBUILT_KERNEL):kernel
+
 -include device/semc/mogami-common/mogami.mk
 
 # These is the hardware-specific overlay, which points to the location
