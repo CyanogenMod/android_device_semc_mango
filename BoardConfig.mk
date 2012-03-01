b73b74ceb48334f731cc9dbe9f118dec7ab7033f
freexperia <jerpelea@gmail.com>
2012-03-01 21:08:58 +0200

    semc: sensor config
    Change-Id: Ie0071c03dfa60ec4dbd5155e4e4da1f27ec27706

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,13 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/mango/BoardConfigVendor.mk
 
+TARGET_SPECIFIC_HEADER_PATH := device/semc/mango/include
+
+SENSORS_COMPASS_AK897X := true
+SENSORS_ACCEL_BMA150_INPUT := false
+SENSORS_ACCEL_BMA250_INPUT := true
+SENSORS_PROXIMITY_APDS970X := true
+SENSORS_PRESSURE_BMP180 := false
+
 TARGET_OTA_ASSERT_DEVICE := SK17i,SK17a,mango
 
