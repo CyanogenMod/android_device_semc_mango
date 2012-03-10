eb9487bdb72c6403b6f0070afb5b9045aa352f23
Diogo Ferreira <defer@cyanogenmod.com>
2012-03-10 17:59:23 +0000

    mango: Copy dpi-dependent resources using a generic makefile from msm7x30-common
    Change-Id: I0b1b759dfd0a81217f37b9b1dd24f230dcd2b728

--- device_mango.mk
+++ device_mango.mk
@@ -39,15 +39,7 @@ PRODUCT_COPY_FILES += \
 # semc msm7x30 uses high-density artwork where available
 PRODUCT_LOCALES += mdpi
 
-#Offline charging animation
-PRODUCT_COPY_FILES += \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_01_M.png:system/semc/chargemon/data/charging_animation_01.png \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_02_M.png:system/semc/chargemon/data/charging_animation_02.png \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_03_M.png:system/semc/chargemon/data/charging_animation_03.png \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_04_M.png:system/semc/chargemon/data/charging_animation_04.png \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_05_M.png:system/semc/chargemon/data/charging_animation_05.png \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_06_M.png:system/semc/chargemon/data/charging_animation_06.png \
-    device/semc/msm7x30-common/prebuilt/animations/charging_animation_07_M.png:system/semc/chargemon/data/charging_animation_07.png
+-include device/semc/msm7x30-common/prebuilt/resources-mdpi.mk
 
 #LCD DENSITY
 PRODUCT_PROPERTY_OVERRIDES += \
