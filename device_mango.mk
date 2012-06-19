62715f0ec3286068c5a6e1a1559c33b1790a5215
freexperia <jerpelea@gmail.com>
2012-06-19 18:38:45 +0300

    mango: camera permission

--- device_mango.mk
+++ device_mango.mk
@@ -31,14 +31,15 @@ PRODUCT_COPY_FILES += \
     device/semc/mango/prebuilt/hw_config.sh:system/etc/hw_config.sh \
     device/semc/mango/prebuilt/pre_hw_config.sh:root/pre_hw_config.sh \
     device/semc/msm7x30-common/prebuilt/logo_M.rle:root/logo.rle \
-    device/semc/msm7x30-common/prebuilt/filler:root/filler2 \
     device/semc/mango/recovery.fstab:root/recovery.fstab \
     device/semc/mogami-common/prebuilt/keyswap:root/sbin/keyswap \
     device/semc/mogami-common/prebuilt/init.semc.keyboard.rc:root/init.semc.keyboard.rc
 
+#    device/semc/msm7x30-common/prebuilt/fillers:root/filler2 \
+
 #device specific features
 PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/frameworks/base/data/etc/android.hardware.camera.front.xml
+    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
 
 # semc msm7x30 uses high-density artwork where available
 PRODUCT_LOCALES += mdpi
