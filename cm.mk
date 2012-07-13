e1d5804de95b3768a93578c5e47337d56ea2ce67
freexperia <jerpelea@gmail.com>
2012-07-13 20:51:01 +0300

    mango: update inclusion

--- cm.mk
+++ cm.mk
@@ -1,22 +1,20 @@
-## Specify phone tech before including full_phone
+# Inherit device configuration
+$(call inherit-product, device/semc/mango/full_mango.mk)
+
+# Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := SK17i
-
-TARGET_BOOTANIMATION_NAME := vertical-320x480
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/semc/mango/device_mango.mk)
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := mango
-PRODUCT_NAME := cm_mango
-PRODUCT_BRAND := SEMC
-PRODUCT_MODEL := Xperia Mini Pro
+# Release name
+PRODUCT_RELEASE_NAME := SK17i
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SK17i BUILD_FINGERPRINT="SEMC/SK17i_1249-7380/SK17i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys" PRIVATE_BUILD_DESC="SK17i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
+
+TARGET_BOOTANIMATION_NAME := vertical-320x480
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := cm_mango
+PRODUCT_DEVICE := mango
--- device_mango.mk
+++ device_mango.mk
@@ -1,17 +1,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# common msm7x30 configs
+$(call inherit-product, device/semc/msm7x30-common/msm7x30.mk)
+
+# common mogami configs
+$(call inherit-product, device/semc/mogami-common/mogami.mk)
+
 $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 
 # proprietary side of the device
 $(call inherit-product-if-exists, vendor/semc/mango/mango-vendor.mk)
-
-
-# Discard inherited values and use our own instead.
-PRODUCT_NAME := mango
-PRODUCT_DEVICE := mango
-PRODUCT_MODEL := mango
-
--include device/semc/mogami-common/mogami.mk
 
 # These is the hardware-specific overlay, which points to the location
 # of hardware-specific resource overrides, typically the frameworks and
@@ -41,12 +39,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
 
-# semc msm7x30 uses high-density artwork where available
-PRODUCT_LOCALES += mdpi
-
--include device/semc/msm7x30-common/prebuilt/resources-mdpi.mk
+$(call inherit-product, device/semc/msm7x30-common/prebuilt/resources-mdpi.mk)
 
 #LCD DENSITY
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=160 \
     com.qc.hdmi_out=false
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
