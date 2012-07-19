51b60b44ada9589ed1cfa57a7de9db3c1c25e804
Jerpelea <jerpelea@gmail.com>
2012-07-19 13:24:15 +0400

    Revert "mango: recovery.fstab path fix"
    This reverts commit 7aa96258955881aa48eaf29b6db04734db5007f5

--- device_mango.mk
+++ device_mango.mk
@@ -29,7 +29,7 @@ PRODUCT_COPY_FILES += \
     device/semc/mango/prebuilt/hw_config.sh:system/etc/hw_config.sh \
     device/semc/mango/prebuilt/pre_hw_config.sh:root/pre_hw_config.sh \
     device/semc/msm7x30-common/prebuilt/logo_M.rle:root/logo.rle \
-    device/semc/mango/recovery.fstab:root/etc/recovery.fstab \
+    device/semc/mango/recovery.fstab:root/recovery.fstab \
     device/semc/mogami-common/prebuilt/keyswap:root/sbin/keyswap \
     device/semc/mogami-common/prebuilt/init.semc.keyboard.rc:root/init.semc.keyboard.rc
 
