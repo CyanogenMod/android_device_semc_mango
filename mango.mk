74ce7be3ddd1ecb751bcd1d34589c9cfbcb8ccec
freexperia <jerpelea@gmail.com>
2012-09-21 22:55:31 +0300

    mango: media profiles cleanup

--- mango.mk
+++ mango.mk
@@ -15,10 +15,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_AAPT_CONFIG := normal mdpi mdpi
 PRODUCT_AAPT_PREF_CONFIG := mdpi
 
-# These are the hardware-specific configuration files
-PRODUCT_COPY_FILES += \
-    device/semc/mango/prebuilt/media_profiles.xml:system/etc/media_profiles.xml
-
 # Init files
 PRODUCT_COPY_FILES += \
     device/semc/mango/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
