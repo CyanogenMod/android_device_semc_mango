bb37620342b737d7c827e42999fe40fc8c5b9cb6
Michael Bestas <mikeioannina@gmail.com>
2012-12-22 20:58:36 +0200

    mango: remove prebuilt kernel
    We build kernel from source since long time
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I02daf1a33b1be85c8f705be448d33c9a2917b70d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,6 @@
 
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := cyanogen_mango_defconfig
-#TARGET_PREBUILT_KERNEL := device/semc/mango/prebuilt/kernel
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
 TARGET_RECOVERY_INITRC := device/semc/mango/recovery/init.rc
@@ -15,4 +14,3 @@ SENSORS_PROXIMITY_APDS9700 := true
 SENSORS_PRESSURE_BMP180 := false
 
 TARGET_OTA_ASSERT_DEVICE := SK17i,SK17a,mango
-
