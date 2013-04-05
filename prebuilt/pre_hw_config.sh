d53524259b83afbb1dbe3fd5b34cf824bceffad7
Michael Bestas <mikeioannina@gmail.com>
2013-04-05 19:30:16 +0300

    mango: Fix leds & backlight configuration on boot
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I686ace45cee09a32a73b21be268b209ce08121d4

--- prebuilt/pre_hw_config.sh
+++
@@ -1,7 +0,0 @@
-# LMU AS3676 Configuration before chargemon
-dev=/sys/devices/i2c-0/0-0040/leds
-echo 500 > $dev/button-backlight/max_current
-echo 21000 > $dev/lcd-backlight/max_current
-echo 5000 > $dev/red/max_current
-echo 5000 > $dev/green/max_current
-echo 5000 > $dev/blue/max_current
